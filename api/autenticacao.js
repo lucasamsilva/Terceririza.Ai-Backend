93de836db1c1381f09909f03ae851788361f9bad
lucasamsilva <lucasandreim280998@gmail.com>
2020-12-17 10:18:26 -0300

    First Commit

--- api/autenticacao.js
+++ api/autenticacao.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const authSecret = process.env.AUTH_SECRET;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jwt-simple')
 
 module.exports = app => {
