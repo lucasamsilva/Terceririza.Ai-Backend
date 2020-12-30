2f0ebcbbfb3ff96c0ecc8607404c411ae2692467
lucasamsilva <lucasandreim280998@gmail.com>
2020-12-30 20:52:35 -0300

    Final e auth

--- config/routes.js
+++ config/routes.js
@@ -9,29 +9,31 @@ module.exports = app => {
     app.post('/validar', app.api.autenticacao.validarToken)
 
     app.route('/empregos')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .post(app.api.empregos.novo)
         .get(app.api.empregos.buscarEmpregos)
 
     app.route('/empregos/:id')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.empregos.buscarEmpregoPorId)
         .put(app.api.empregos.novo)
         .delete(app.api.empregos.excluirEmprego)
 
     app.route('/usuarios/:id')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.usuarios.retornaUsuario)
 
     app.route('/empregos/usuario/:id')
-        // .all(app.config.passport.authenticate())
+        .all(app.config.passport.authenticate())
         .get(app.api.usuarios.retornaVagasUsuario)
 
     app.route('/terceirizado')
+        .all(app.config.passport.authenticate())
         .get(app.api.terceirizados.buscarTerceirizados)
         .post(app.api.terceirizados.novoTerceirizado)
     
     app.route('/terceirizado/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.terceirizados.buscarTerceirizadoPorId)
         .put(app.api.terceirizados.novoTerceirizado)
 
